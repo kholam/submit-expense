1dc0ef2bff4e41d5b6736fd92db0c61d33098212
Khaliddraig <khaliddraig1992@gmail.com>
2022-02-08 03:04:45 +0800

    import fonts with body styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/css/sass/main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
