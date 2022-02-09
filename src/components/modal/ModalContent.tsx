1404853cc047f3c085911ee15e2d2e9a12caff93
Khaliddraig <khaliddraig1992@gmail.com>
2022-02-09 23:59:52 +0800

    set default value for the date

--- src/components/modal/ModalContent.tsx
+++ src/components/modal/ModalContent.tsx
@@ -1,58 +1,64 @@
-import React from 'react';
-import { currencies } from '../../helpers/Utils';
+import React, { useState } from 'react';
+import { currencies, todaysDate } from '../../helpers/Utils';
+import { FormValues } from '../../interface';
 import { Label } from '../form';
 import FormInput from '../form/FormInput';
 import { CalendarIcon } from '../svg';
 
 const ModalContent = (): React.ReactElement => {
+    const initialValues = { title: '', purchaseDate: todaysDate(), currency: '', amount: '' } as FormValues;
+    const [formValues, setFormValues] = useState<FormValues>(initialValues);
+
     return (
         <div className="modal-content">
-        <form name="testForm">
-            {/* title */}
-            <div className="form-title">
-                <Label text="Title" />
-                <FormInput name="title" placeholder="Expense Title" required className="input-text" />
-            </div>
-            {/* purchase date */}
-            <div className="form-purchase-date">
-                <Label text="Purchase Date" />
-                <div className="input-date-picker">
-                    <FormInput name="purchaseDate"
-                        required type="date"
+            <form name="testForm">
+                {/* title */}
+                <div className="form-title">
+                    <Label text="Title" />
+                    <FormInput name="title" placeholder="Expense Title" required className="input-text" />
+                </div>
+                {/* purchase date */}
+                <div className="form-purchase-date">
+                    <Label text="Purchase Date" />
+                    <div className="input-date-picker">
+                        <FormInput name="purchaseDate"
+                            required
+                            type="date"
+                            value={formValues.purchaseDate}
+                        />
+                        <CalendarIcon className="calendar-icon" />
+                    </div>
+                </div>
+                {/* currency selection */}
+                <div className="form-currency">
+                    <div className="flex-row">
+                        <Label text="Currency" className="float-right" />
+                        <select className="select" name="currency">
+                            <option value=""> </option>
+                            {
+                                currencies.map((currency: string, index: number) => {
+                                    return (
+                                        <option value={`${currency.toLowerCase()}`} key={index}>
+                                            {currency}
+                                        </option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </div>
+                </div>
+                {/* total amount */}
+                <div className="form-amount">
+                    <Label text="Amount" />
+                    <FormInput
+                        name="amount"
+                        placeholder="Total Amount"
+                        required
+                        className="input-text"
+                        type="number"
                     />
-                    <CalendarIcon className="calendar-icon" />
                 </div>
-            </div>
-            {/* currency selection */}
-            <div className="form-currency">
-                <div className="flex-row">
-                    <Label text="Currency" className="float-right" />
-                    <select className="select" name="currency">
-                        <option value=""> </option>
-                        {
-                            currencies.map((currency: string, index: number) => {
-                                return (
-                                    <option value={`${currency.toLowerCase()}`} key={index}>
-                                        {currency}
-                                    </option>
-                                )
-                            })
-                        }
-                    </select>
-                </div>
-            </div>
-            {/* total amount */}
-            <div className="form-amount">
-                <Label text="Amount" />
-                <FormInput
-                    name="amount"
-                    placeholder="Total Amount"
-                    required
-                    className="input-text"
-                    type="number"
-                />
-            </div>
-        </form>
+            </form>
         </div>
     )
 };
--- src/helpers/Utils.ts
+++ src/helpers/Utils.ts
@@ -1,1 +1,7 @@
 export const currencies = ['EUR', 'USD', 'CNY'];
+
+
+// returns formatted string of the current date in ->  dd-mm-yyyy
+export const todaysDate = (): string => {
+    return new Date().toISOString().split('T')[0];
+}
