46113f36087086f5455682b12ac40c27f38edf5d
Khaliddraig <khaliddraig1992@gmail.com>
2022-02-09 23:53:22 +0800

    modal form to modal content with css styling

--- src/components/modal/ModalContent.tsx
+++ src/components/modal/ModalContent.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { currencies } from '../../helpers/Utils';
+import { Label } from '../form';
+import FormInput from '../form/FormInput';
+import { CalendarIcon } from '../svg';
 
-const ModalContent = (): React.ReactElement =>{
+const ModalContent = (): React.ReactElement => {
     return (
-        <>
-        </>
+        <div className="modal-content">
+        <form name="testForm">
+            {/* title */}
+            <div className="form-title">
+                <Label text="Title" />
+                <FormInput name="title" placeholder="Expense Title" required className="input-text" />
+            </div>
+            {/* purchase date */}
+            <div className="form-purchase-date">
+                <Label text="Purchase Date" />
+                <div className="input-date-picker">
+                    <FormInput name="purchaseDate"
+                        required type="date"
+                    />
+                    <CalendarIcon className="calendar-icon" />
+                </div>
+            </div>
+            {/* currency selection */}
+            <div className="form-currency">
+                <div className="flex-row">
+                    <Label text="Currency" className="float-right" />
+                    <select className="select" name="currency">
+                        <option value=""> </option>
+                        {
+                            currencies.map((currency: string, index: number) => {
+                                return (
+                                    <option value={`${currency.toLowerCase()}`} key={index}>
+                                        {currency}
+                                    </option>
+                                )
+                            })
+                        }
+                    </select>
+                </div>
+            </div>
+            {/* total amount */}
+            <div className="form-amount">
+                <Label text="Amount" />
+                <FormInput
+                    name="amount"
+                    placeholder="Total Amount"
+                    required
+                    className="input-text"
+                    type="number"
+                />
+            </div>
+        </form>
+        </div>
     )
-}; 
+};
 
 export default ModalContent;
---
+++ src/helpers/Utils.ts
@@ -0,0 +1,1 @@
+export const currencies = ['EUR', 'USD', 'CNY'];
