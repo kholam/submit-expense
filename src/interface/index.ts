592d8a3b364ab2cc50b967e5435339c23ba42365
Khaliddraig <khaliddraig1992@gmail.com>
2022-02-09 23:10:51 +0800

    allow export of additional props

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,9 +1,13 @@
 import SubmitExpenseModalProps from "./SubmitExpenseModalProps";
 import SVGProps from "./SVGProps";
 import ModalProps from "./ModalProps";
+import FormInputProps from "./form/FormInputProps";
+import FormValues from "./form/FormValues";
 
 export type {
     SubmitExpenseModalProps,
     SVGProps,
     ModalProps,
+    FormInputProps,
+    FormValues
 }
