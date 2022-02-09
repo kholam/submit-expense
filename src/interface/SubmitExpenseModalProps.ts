da46774f73d2e2cf6e7e2a6093ba7c65c7a95bee
Khaliddraig <khaliddraig1992@gmail.com>
2022-02-09 21:58:05 +0800

    props for modal

---
+++ src/interface/SubmitExpenseModalProps.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// props for submit expense modal
+interface SubmitExpenseModalProps {
+ displayModal: boolean;
+ setShowDisplayModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default SubmitExpenseModalProps;
