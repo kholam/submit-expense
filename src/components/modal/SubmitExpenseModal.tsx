4a90481d39fa19aa39df6e201712d611c354e7ab
Khaliddraig <khaliddraig1992@gmail.com>
2022-02-09 23:03:33 +0800

    add footer to modal

--- src/components/modal/SubmitExpenseModal.tsx
+++ src/components/modal/SubmitExpenseModal.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { ModalHeader } from ".";
+import { ModalFooter, ModalHeader } from ".";
 import { SubmitExpenseModalProps } from "../../interface";
 
 const SubmitExpenseModal = ({ displayModal, setShowDisplayModal }: SubmitExpenseModalProps): React.ReactElement => {
+
+    const handleCloseModal = () => {
+        setShowDisplayModal(!displayModal);
+    }
 
     return (
         <React.Fragment>
             {displayModal && <div className="modal">
                 <ModalHeader displayModal={displayModal} setShowDisplayModal={setShowDisplayModal}/>
+                <ModalFooter handleCloseModal={handleCloseModal}/>
             </div>}
         </React.Fragment>
     )
--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -1,7 +1,9 @@
 import SubmitExpenseModal from "./SubmitExpenseModal";
 import ModalHeader from "./ModalHeader";
+import ModalFooter from "./ModalFooter";
 
 export {
     SubmitExpenseModal,
-    ModalHeader
+    ModalHeader,
+    ModalFooter,
 }
