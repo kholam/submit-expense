73759daec6aaa4c18a7572c6013a9fac34c52126
Khaliddraig <khaliddraig1992@gmail.com>
2022-02-10 00:54:34 +0800

    handle click for next button

--- src/components/modal/ModalFooter.tsx
+++ src/components/modal/ModalFooter.tsx
@@ -3,11 +3,18 @@ import { ModalProps } from '../../interface';
 
 const ModalFooter = ({ handleCloseModal, isFormValid } : ModalProps)=>{
 
+    // perform the necessary action and proceed to the next step
+    const handleClickNext = (event: React.MouseEvent<HTMLButtonElement>) =>{
+    // form values can be accessed here 
+    // create a context and store form values in for access here
+        event.preventDefault();
+    }
+
     return (
         <div className="modal-footer">
             <div className="modal-buttons">
                 <button className="btn btn-outline mr-12" onClick={handleCloseModal}>Cancel</button>
-                <button className={`btn btn-primary ${!isFormValid ? 'disabled' : ''}`}>Next</button>
+                <button className={`btn btn-primary ${!isFormValid ? 'disabled' : ''}`} onClick={handleClickNext}>Next</button>
             </div>
         </div>
     )
